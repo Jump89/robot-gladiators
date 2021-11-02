4822277b0e9c0e70ba1d98742ad3dba9a12b8cd8
Marcio R <Murcielago203@gmail.com>
2021-11-02 15:42:19 -0400

    Gave variables to enemy and player started first round

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,46 @@
 // this creates a function named "fight"
 var playerName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// you can also log multipe values at once like this 
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+
+// Alert players that they are starting the round.
+window.alert("Welcome to Robot Gladiators!");
+// Subtract the Value of playerAttack from the value of enemyHealth and use the result tp u[date the value in tge enemyHealth variable
+enemyHealth = enemyHealth - playerAttack;
+// log a resulting message to the console so we know that it worked.
+console.log(
+    playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth  + " health remaining. "
+);
+
+//Subtract the value of enemyAttack from the value of playerHealth and use the result to update the value in the playerHealth variable.
+playerHealth = playerHealth - enemyAttack;
+
+// log a resulting message to the console so we know that it worked. 
+console.log(
+    enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining "
+);
+};
+
+fight();
+
+
+
 // Note the lack of quotation marks around playerName window. alert(playerName)
 // what is this ?
-console.log("This logs a string, good for leaving yourself a message");
+//console.log("This logs a string, good for leaving yourself a message");
 //this will do math and log 20
-console.log(10+10);
+//console.log(10+10);
 
-function fight() {
+/*function fight() {
     window.alert ("The fight has begun!");
 }
 
@@ -18,7 +52,7 @@ console.log("Jump the robot" + " is ready for battle!")
 console.log(playerName + " is ready for battle!");
 
 //" Your robot , Jump  the robot , has won!"
-console.log("Your robot," + playerName +", has won!");
+console.log("Your robot," + playerName +", has won!");*/
 
 
 
