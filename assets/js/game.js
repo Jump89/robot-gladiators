5b005ddcf0bf00e20bf6af5536a8ea1745d93eee
Marcio R <Murcielago203@gmail.com>
2021-11-02 15:04:14 -0400

    Add fighters names and console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,32 @@
-window.alert("this an alert. to see if JavaScript is running")
+// this creates a function named "fight"
+var playerName = window.prompt("what is your robot's name?");
+// Note the lack of quotation marks around playerName window. alert(playerName)
+// what is this ?
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+
+function fight() {
+    window.alert ("The fight has begun!");
+}
+
+var playerName = "Jump the robot";
+// Jump the robot is ready to battle!
+console.log("Jump the robot" + " is ready for battle!")
+
+// " Jump the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+//" Your robot , Jump  the robot , has won!"
+console.log("Your robot," + playerName +", has won!");
+
+
+
+
+
+
+
+
+
+//fight();
+
