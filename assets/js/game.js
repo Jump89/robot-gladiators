ab94951146f9b2b8acbe878e67d030e0f22b97c9
Marcio R <Murcielago203@gmail.com>
2021-11-04 16:23:01 -0400

    Fixed valid name slection promt

--- assets/js/game.js
+++ assets/js/game.js
@@ -78,7 +78,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-
+        
         // pick new enemy to fight based on the index of the enemyNames array
         var pickedEnemyObj = enemyInfo[i];
 
@@ -158,8 +158,20 @@ var shop = function() {
   }
 };
 
-var playerInfo= {
-  name: window.prompt("What is your robot's name?"),
+// function to set name 
+var getPlayerName = function(){
+  var name = "";
+ // loop here with prompt and condition 
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+};
+
+
+var playerInfo = {
+  name: getPlayerName(),
   health: 100,
   attack: 50,
   money: 10,
